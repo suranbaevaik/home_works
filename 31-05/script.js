d2141212c8233f7a79547e05576cbb6ed63345c0
Aikanysh <suranbaeva.aik@gmail.com>
2022-06-02 18:08:51 +0600

    feat: add js code

---
+++ 31-05/script.js
@@ -0,0 +1,19 @@
+let userSalaries = {
+    Ella: 10000,
+    Sophia: 15000,
+    Ellie: 20000
+}
+
+alert(userSalaries.Ella + userSalaries.Sophia);
+
+let point = {
+    a: 5,
+    b: 6,
+    c: 10
+}
+
+alert(point.a + point.b + point.c);
+
+let data = 5 > 10;
+let number = data === true ? '3' : '5';
+alert(number);
