d707651920141bc02435d3ed99cfc4367f0f6a00
Aikanysh <suranbaeva.aik@gmail.com>
2022-06-18 17:50:01 +0600

    init: start

---
+++ 16-06/script.js
@@ -0,0 +1,12 @@
+/*********** 1st TASK ***********/
+
+let arr = [2, 3, 4, 5, 6, 7];
+let result = 1;
+for (let i = 0; i < arr.length ; i++) {
+    result *= arr[i];
+}
+
+console.log(result);
+
+/*********** 2nd TASK ***********/
+
