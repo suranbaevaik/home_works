d9edfa4c5b410af0b90370e2f29ad6ef042c1a98
Aikanysh <suranbaeva.aik@gmail.com>
2022-06-18 14:59:47 +0600

    fix: script.js

--- 04-06/script.js
+++ 04-06/script.js
@@ -27,6 +27,6 @@ console.log(result);
 let arr3 = [1,2,3,4,5,6,7,8,9,10];
 let square = 0;
 for (let i = 0; i < arr3.length; i++) {
-    square += arr3[i]**2;
+    square += arr3[i] ** 2;
 }
 console.log(square);
