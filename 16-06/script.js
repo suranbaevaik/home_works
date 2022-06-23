2cdc47999530909cce7a53d7cdf6b306ddd71d51
Aikanysh <suranbaeva.aik@gmail.com>
2022-06-23 15:41:15 +0600

    fix: task_2

--- 16-06/script.js
+++ 16-06/script.js
@@ -10,3 +10,14 @@ console.log(result);
 
 /*********** 2nd TASK ***********/
 
+const reverseArray = (data) => {
+    const newArray = [];
+    for (let i = data.length - 1; i >= 0; i--) {
+        newArray.push(data[i]);
+    }
+    return newArray;
+}
+
+const myArray = ['hello', 20, 21, true];
+const result_2 = reverseArray(myArray);
+console.log(result_2);
