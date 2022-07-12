9174008220dace5af51a4a05f4a26159dc2cb71d
Aikanysh <suranbaeva.aik@gmail.com>
2022-07-12 13:23:02 +0600

    fix: homework 07-07

--- 07-07/script.js
+++ 07-07/script.js
@@ -1,18 +1,37 @@
 const btn = document.querySelector('#btn');
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+
 function showNotification(options) {
     let information = document.createElement('div');
+    console.log(information);
     information.className = 'notification';
     information.innerText = options.html;
+    information.className = options.className += ' notification';
     document.body.append(information);
     setTimeout(() => information.remove(), 1500)
 }
 
 btn.addEventListener('click', () => {
     showNotification({
-        html: 'A simple primary alert—check it out!',
+        html: 'A simple success alert—check it out!',
+        className: 'success',
     });
 })
 
+btn1.addEventListener('click', () => {
+    showNotification({
+        html: 'A simple info alert—check it out!',
+        className: 'info',
+    })
+})
+
+btn2.addEventListener('click', () => {
+    showNotification({
+        html: 'A simple danger alert—check it out!',
+        className: 'error',
+    })
+})
 
 
 
